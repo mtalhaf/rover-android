79732787c3557ce7730dadd2ba981d4985c76028
mtalhaf93 <mtalhaf@gmail.com>
2017-02-25 21:21:16 +0000

    removed check for network state permission.

--- app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
@@ -1,9 +1,6 @@
 package com.rovercontroller.mtalhaf.rovercontroller.activity;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -40,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setUpViews();
         setUpViewListeners();
-        checkForPermissions();
     }
 
     @Override
@@ -67,34 +63,6 @@ public class MainActivity extends AppCompatActivity {
                 sendMessage();
             }
         });
-    }
-
-    private void checkForPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_NETWORK_STATE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_NETWORK_STATE)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
-                        MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
     }
 
     @Override
