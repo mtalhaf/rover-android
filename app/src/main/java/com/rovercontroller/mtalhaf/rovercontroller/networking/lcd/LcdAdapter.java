468cd594d539de80379ac383a781fc7129163c2b
mtalhaf93 <mtalhaf@gmail.com>
2017-02-25 21:14:07 +0000

    update renamed file with capital letters (add git configuration).

---
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/networking/lcd/LcdAdapter.java
@@ -0,0 +1,13 @@
+package com.rovercontroller.mtalhaf.rovercontroller.networking.lcd;
+
+import java.util.Map;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by Study on 2/25/2017.
+ */
+
+public interface LcdAdapter {
+    Observable<String> displayMessage(Map<String, String> options);
+}
---
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/networking/lcd/LcdServerAdapter.java
@@ -0,0 +1,29 @@
+package com.rovercontroller.mtalhaf.rovercontroller.networking.lcd;
+
+import android.content.Context;
+
+import com.rovercontroller.mtalhaf.rovercontroller.networking.BaseAdapter;
+import com.rovercontroller.mtalhaf.rovercontroller.networking.RestfulRetrofitService;
+
+import java.util.Map;
+
+import io.reactivex.Observable;
+
+/**
+ * Created by Study on 2/25/2017.
+ */
+
+public class LcdServerAdapter implements LcdAdapter {
+
+    RestfulRetrofitService service;
+
+    public LcdServerAdapter(Context context){
+        BaseAdapter baseAdapter = new BaseAdapter(context);
+        service = baseAdapter.getService();
+    }
+
+    @Override
+    public Observable<String> displayMessage(Map<String, String> options) {
+        return service.displayMessage(options);
+    }
+}
