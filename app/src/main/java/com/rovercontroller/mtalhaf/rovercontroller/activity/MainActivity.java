0a7b4905e5da7c253a31f5afb588d9e06dec88eb
mtalhaf93 <mtalhaf@gmail.com>
2017-02-25 21:21:38 +0000

    remove callback for permission .

--- app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
@@ -65,32 +65,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }
-
-
     private void setUpVariables() {
         lcdAdapter = new LcdServerAdapter(this.getApplicationContext());
     }
