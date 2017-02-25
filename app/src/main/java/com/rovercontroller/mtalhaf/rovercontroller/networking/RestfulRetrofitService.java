a2e7b953195af39e2be2a49aeb139819b26a9f68
mtalhaf93 <mtalhaf@gmail.com>
2017-02-25 19:36:24 +0000

    added enpoint for displaying the message on the lcd.

--- app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/networking/RestfulRetrofitService.java
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/networking/RestfulRetrofitService.java
@@ -1,8 +1,18 @@
 package com.rovercontroller.mtalhaf.rovercontroller.networking;
 
+import java.util.Map;
+
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
+import rx.Observable;
+
 /**
  * Created by Study on 2/25/2017.
  */
 
 public interface RestfulRetrofitService {
+
+    @GET("v1.0/lcd/print")
+    Observable<String> displayMessage(@QueryMap Map<String, String> options);
 }
