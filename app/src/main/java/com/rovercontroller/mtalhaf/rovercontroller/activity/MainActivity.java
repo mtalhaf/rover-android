2859858cd9881f1feef1c3ee5b93a901218ec79e
mtalhaf93 <mtalhaf@gmail.com>
2017-02-25 21:09:39 +0000

    added permission and permission check for network access state.

--- app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/activity/MainActivity.java
@@ -1,5 +1,9 @@
 package com.rovercontroller.mtalhaf.rovercontroller.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +26,7 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE = 100;
     EditText displayMessageEditText;
     Button displayMessageButton;
 
@@ -35,10 +40,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setUpViews();
         setUpViewListeners();
+        checkForPermissions();
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         setUpVariables();
     }
@@ -63,13 +69,67 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void checkForPermissions() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_NETWORK_STATE)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                        MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+
     private void setUpVariables() {
         lcdAdapter = new LcdServerAdapter(this.getApplicationContext());
     }
 
     private void cleanUp() {
         if (displayMessageDisposable != null)
-            if(!displayMessageDisposable.isDisposed())
+            if (!displayMessageDisposable.isDisposed())
                 displayMessageDisposable.dispose();
     }
 
