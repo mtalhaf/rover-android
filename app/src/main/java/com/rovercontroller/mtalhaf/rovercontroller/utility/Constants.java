3250a014a9ad1108257a3ecdc2df0933af9aa171
mtalhaf93 <mtalhaf@gmail.com>
2017-02-27 16:01:27 +0000

    added constants file.

---
+++ app/src/main/java/com/rovercontroller/mtalhaf/rovercontroller/utility/Constants.java
@@ -0,0 +1,11 @@
+package com.rovercontroller.mtalhaf.rovercontroller.utility;
+
+/**
+ * Created by Study on 2/27/2017.
+ * This class contains all the constants used by the main activity
+ */
+
+public class Constants {
+    public static final String ROVER_TURN_LEFT = "ROVER_TURN_LEFT";
+    public static final String ROVER_TURN_RIGHT = "ROVER_TURN_RIGHT";
+}
